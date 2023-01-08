aeb380ea4a9390461db7b1c9a9e48a758db5242d
lazaronazareno <lazaronazareno@gmail.com>
2023-01-08 18:03:40 -0300

    formatting and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Proyects from './components/proyects'
 
 function App () {
   return (
-    <h1>MERNTasks</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Login />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/proyects' element={<Proyects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
