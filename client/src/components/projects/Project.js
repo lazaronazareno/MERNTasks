624be171e7cca3de37a4832d66e3887f1dfe7c25
lazaronazareno <lazaronazareno@gmail.com>
2023-01-09 13:58:41 -0300

    usecontext in projects components

--- client/src/components/projects/Project.js
+++ client/src/components/projects/Project.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ProjectContext from '../../context/projects/projectContext'
 
 const Project = ({ project }) => {
+  const projectsContext = useContext(ProjectContext)
+  const { setCurrentProject } = projectsContext
+
   return (
     <li className='d-flex'>
       <button
         className='btn btn-link'
+        onClick={() => setCurrentProject(project.id)}
       >{project.name}
       </button>
     </li>
--- client/src/components/projects/ProjectList.js
+++ client/src/components/projects/ProjectList.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import Project from './Project'
+import ProjectContext from '../../context/projects/projectContext'
 
 const ProjectList = () => {
-  const projects = [
-    { name: 'Tienda' },
-    { name: 'Internet' },
-    { name: 'Diseño Portfolio' }
-  ]
+  const projectsContext = useContext(ProjectContext)
+  const { projects, getProjects } = projectsContext
+
+  useEffect(() => {
+    getProjects()
+  }, [])
+
+  if (projects.length === 0) return <p>No hay projectos, comienza creando uno.</p>
+
   return (
     <ul>
       {projects.map(project => (
